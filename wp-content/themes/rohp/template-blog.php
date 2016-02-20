e65527c1a85801f896c10ad255e8484dd652733f
Igor Sertić <utzyyy@gmail.com>
2016-02-20 04:01:15 +0100

    Blog list style + pagination -  Done.

--- wp-content/themes/rohp/template-blog.php
+++ wp-content/themes/rohp/template-blog.php
@@ -5,25 +5,25 @@
 ?>
 <?php get_header(); ?>
 <?php
-    $args = array(
-        'posts_per_page' => 10,
-        'paged' => $paged
+$args = array(
+    'posts_per_page' => 5,
+    'paged' => $paged
 
-    );
+);
 
-    $wp_query = new WP_Query($args);
+$wp_query = new WP_Query($args);
 ?>
-<div class="gutter">
+    <div class="gutter">
 
-    <?php
-    if ( has_post_thumbnail() ) :
-        $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-        ?>
-        <div class="featuredImage" style="background-image: url('<?php echo $image[0];?>')">
-            <?php //the_post_thumbnail(); ?>
-            <!--img src="images/featured.jpg"-->
-        </div>
-    <?php endif; ?>
+<?php
+if ( has_post_thumbnail() ) :
+    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+    ?>
+    <div class="featuredImage" style="background-image: url('<?php echo $image[0];?>')">
+        <?php //the_post_thumbnail(); ?>
+        <!--img src="images/featured.jpg"-->
+    </div>
+<?php endif; ?>
 
     <div class="content">
         <div class="row no-margin no-padding">
@@ -39,42 +39,45 @@
                     <?php if($wp_query->have_posts()): while($wp_query->have_posts()): $wp_query->the_post(); ?>
 
 
-                    <!-- Blog post -->
-                    <div class="post-item" id="post-<?php the_ID(); ?>">
-                        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                        <hr>
-                        <span class="post-date"><?php echo get_the_date(); ?> | </span>
-                        <span class="post-author">POST BY <?php echo get_the_author(); ?> | </span>
-                        <span class="post-cat">IN
+                        <!-- Blog post -->
+                        <div class="post-item" id="post-<?php the_ID(); ?>">
+                            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                            <div class="post-divider"></div>
+                            <span class="post-date"><?php echo get_the_date(); ?> | </span>
+                            <span>Post by</span> <span class="post-author"><?php echo get_the_author(); ?></span> |
+                            <span>In</span><span class="post-cat">
 
                         <?php
-                            $categories = get_the_category();
-                            $separator = ' ';
-                            $output = '';
-                            if ( ! empty( $categories ) ) {
-                                foreach( $categories as $category ) {
-                                    $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
-                                }
-                                echo trim( $output, $separator );
+                        $categories = get_the_category();
+                        $separator = ' ';
+                        $output = '';
+                        if ( ! empty( $categories ) ) {
+                            foreach( $categories as $category ) {
+                                $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
                             }
+                            echo trim( $output, $separator );
+                        }
                         ?>
                        </span><br>
-                       <div class="introtext"><?php echo substr(get_the_excerpt(), 0 , 256)."..."; ?></div>
-                       <a href="<?php the_permalink(); ?>" class="more">Read More</a>
-                    </div>
-                    <!-- Blog post end -->
+                            <div class="post-intro">
+                                <?php echo substr(get_the_excerpt(), 0 , 256)."..."; ?>
+                                <a href="<?php the_permalink(); ?>" class="post-more">read more >></a>
+                            </div>
+
+                        </div>
+                        <!-- Blog post end -->
                     <?php endwhile;?>
 
 
-                    <?php
-                    the_posts_pagination( array(
-                        'screen_reader_text' => ' ',
-                        'mid_size'           => 2,
-                        'prev_text'          => __( 'prev', 'richmondoval' ),
-                        'next_text'          => __( 'next', 'richmondoval' ),
-                        //'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( '', 'richmondoval' ) . ' </span>',
-                    ) );
-                    ?>
+                        <?php
+                        the_posts_pagination( array(
+                            'screen_reader_text' => ' ',
+                            'mid_size'           => 2,
+                            'prev_text'          => __( 'prev', 'richmondoval' ),
+                            'next_text'          => __( 'next', 'richmondoval' ),
+                            //'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( '', 'richmondoval' ) . ' </span>',
+                        ) );
+                        ?>
                     <?php endif; ?><?php wp_reset_postdata();?>
 
                 </div>
@@ -93,16 +96,16 @@
                             </ul>
                         </div>
                     <?php } else { ?>
-                    <div class="quick-links">
-                        <h4><i class="fa fa-link"></i> Quick links</h4>
-                        <ul>
-                            <li><a href="/contact" title="Contact Us">Contact Us</a></li>
-                            <li><a href="/about-us/team" title="High Performance Team ">High Performance Team</a></li>
-                            <li><a href="/about-us/training-philosophy/" title="Training Philosophy">Training Philosophy</a></li>
-                            <li><a href="/strength-conditioning/sc-individual-training/individual-training/" title="Private Strength & Conditioning Training">Private Strength & Conditioning Training</a></li>
-                        </ul>
-                    </div>
-                        <?php } ?>
+                        <div class="quick-links">
+                            <h4><i class="fa fa-link"></i> Quick links</h4>
+                            <ul>
+                                <li><a href="/contact" title="Contact Us">Contact Us</a></li>
+                                <li><a href="/about-us/team" title="High Performance Team ">High Performance Team</a></li>
+                                <li><a href="/about-us/training-philosophy/" title="Training Philosophy">Training Philosophy</a></li>
+                                <li><a href="/strength-conditioning/sc-individual-training/individual-training/" title="Private Strength & Conditioning Training">Private Strength & Conditioning Training</a></li>
+                            </ul>
+                        </div>
+                    <?php } ?>
 
                     <!--div class="textWidget">
                         <p><img src="/wp-content/uploads/2015/08/volleyball-canada.png"></p>
